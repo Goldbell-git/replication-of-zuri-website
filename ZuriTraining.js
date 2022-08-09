87580f54382c41b892db33674624fface1916833
Goldbell-git <78735380+Goldbell-git@users.noreply.github.com>
2022-08-09 20:38:16 +0100

    Add files via upload

---
+++ ZuriTraining.js
@@ -0,0 +1,15 @@
+function mybtn() {
+    document.getElementById('tog-para').classList.toggle('show');
+}
+function mybtn1() {
+    document.getElementById('tog-para1').classList.toggle('show');
+}
+function mybtn2() {
+    document.getElementById('tog-para2').classList.toggle('show');
+}
+function mybtn3() {
+    document.getElementById('tog-para3').classList.toggle('show');
+}
+function mybtn4() {
+    document.getElementById('tog-para4').classList.toggle('show');
+}
